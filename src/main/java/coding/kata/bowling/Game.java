7eefcb67908fb495bd5913066afbe1671152c909
Arthy Vijayaraghavan <arthy.vijayaraghavan@philips.com>
2018-01-05 22:46:00 +0100

    Strike - take next roll's value as well

--- src/main/java/coding/kata/bowling/Game.java
+++ src/main/java/coding/kata/bowling/Game.java
@@ -2,9 +2,14 @@ package coding.kata.bowling;
 
 public class Game {
     private int score;
+    private int previousRoll = 0;
 
     public void roll(int pinsKnockedDown) {
         score += pinsKnockedDown;
+        if (previousRoll == 10) {
+            score += pinsKnockedDown;
+        }
+        previousRoll = pinsKnockedDown;
     }
 
     public int score() {
--- src/test/java/coding/kata/bowling/GameTest.java
+++ src/test/java/coding/kata/bowling/GameTest.java
@@ -9,11 +9,19 @@ public class GameTest {
     private Game game = new Game();
 
     @Test
-    public void knockLessThan10PinsInARoll() {
+    public void rollsWithNoStrikeOrSpare() {
         game.roll(7);
         assertEquals(7, game.score());
         game.roll(6);
         assertEquals(13, game.score());
+    }
+
+    @Test
+    public void strikeRoll() {
+        game.roll(10);
+        assertEquals(10, game.score());
+        game.roll(7);
+        assertEquals(24, game.score());
     }
 
 }
