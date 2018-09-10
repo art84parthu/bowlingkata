312755844d54f3baf01d567803995aa55c2c1077
Arthy Vijayaraghavan <arthy.vijayaraghavan@philips.com>
2018-09-10 23:24:02 +0200

    Bowling kata

--- src/main/java/coding/kata/bowling/Game.java
+++ src/main/java/coding/kata/bowling/Game.java
@@ -1,18 +1,54 @@
 package coding.kata.bowling;
 
 public class Game {
-    private int score;
-    private int previousRoll = 0;
+    private int[] rolls = new int[21];
+    private int rollIndex = 0;
 
     public void roll(int pinsKnockedDown) {
-        score += pinsKnockedDown;
-        if (previousRoll == 10) {
-            score += pinsKnockedDown;
-        }
-        previousRoll = pinsKnockedDown;
+        rolls[rollIndex++] = pinsKnockedDown;
     }
 
     public int score() {
+        int score = 0;
+        int rollIndex = 0;
+        for (int frame = 0; frame < 10; frame++) {
+            if (isStrike(rollIndex)) {
+                score += getStrikeScore(rollIndex);
+            } else if (isSpare(rollIndex)) {
+                score += getSpareScore(rollIndex);
+            } else {
+                score += getFrameScore(rollIndex);
+            }
+            rollIndex = getIndexOfRollInNextFrame(rollIndex);
+        }
         return score;
+    }
+
+    private boolean isStrike(int i) {
+        return rolls[i] == 10;
+    }
+
+    private int getIndexOfRollInNextFrame(int i) {
+        if (isStrike(i)) {
+            return i + 1;
+        } else {
+            return i + 2;
+        }
+    }
+
+    private int getFrameScore(int i) {
+        return rolls[i] + rolls[i + 1];
+    }
+
+    private int getSpareScore(int i) {
+        return (10 + rolls[i + 2]);
+    }
+
+    private int getStrikeScore(int i) {
+        return (10 + rolls[i + 1] + rolls[i + 2]);
+    }
+
+    private boolean isSpare(int i) {
+        return getFrameScore(i) == 10;
     }
 }
--- src/test/java/coding/kata/bowling/GameTest.java
+++ src/test/java/coding/kata/bowling/GameTest.java
@@ -9,19 +9,50 @@ public class GameTest {
     private Game game = new Game();
 
     @Test
-    public void rollsWithNoStrikeOrSpare() {
-        game.roll(7);
-        assertEquals(7, game.score());
-        game.roll(6);
-        assertEquals(13, game.score());
+    public void test0Score() {
+        doRolls(20, 0);
+        assertEquals(0, game.score());
     }
 
     @Test
-    public void strikeRoll() {
-        game.roll(10);
-        assertEquals(10, game.score());
-        game.roll(7);
-        assertEquals(24, game.score());
+    public void test1Score() {
+        doRolls(20, 1);
+        assertEquals(20, game.score());
     }
 
+    @Test
+    public void testSpare() {
+        rollSpare();
+        doRolls(18, 1);
+        assertEquals(29, game.score());
+    }
+
+
+    @Test
+    public void testStrike() {
+        rollStrike();
+        doRolls(18, 1);
+        assertEquals(30, game.score());
+    }
+
+    @Test
+    public void testAllStrikes() {
+        doRolls(12, 10);
+        assertEquals(300, game.score());
+    }
+
+    private void rollStrike() {
+        game.roll(10);
+    }
+
+    private void rollSpare() {
+        game.roll(8);
+        game.roll(2);
+    }
+
+    private void doRolls(int numberOfTimes, int pinsKnockedDown) {
+        for (int i = 0; i < numberOfTimes; i++) {
+            game.roll(pinsKnockedDown);
+        }
+    }
 }
